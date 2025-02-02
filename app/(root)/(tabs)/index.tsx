6f9956e475b69b0fb55cb152bdad02398bb056a0
muhammedzain913 <zainmuhammed409@gmail.com>
2025-02-02 13:53:41 +0530

    AddSomeStylesToHomeScreen

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-lg my-20">Welcome To Restate</Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>  
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
+
+    </View>
+  );
+}
